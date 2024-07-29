61c684c671933fef7061ab4a41a584d12204b86a
KOHYEONJEONG <080821@naver.com>
2024-07-29 23:16:44 +0900

    JPA 옵션

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -26,17 +26,27 @@ public class JpaMain {
         team.setName("TeamA");
         em.persist(team);
 
+        //비영속성
         Member member = new Member();
         member.setUsername("member1");
         member.setTeam(team); //(중요) jpa가 알아서 Team에서 pk를 꺼내서 fk 값에 인서트할때 사용
+
+        //영속성
         em.persist(member);
 
+        /**
+         * 플러시(FLUSH)
+         * 순서1) INSERT 쿼리가 작성 됨.
+         * 순서2) 데이터베이스 트랜직션이 커밋 됨.
+         *
+         * 쓰기 지연 SQL 저장소에 있던 영속성 컨텍스트 데이터가 db에 반영된다.
+         * */
         em.flush();
         em.clear();
 
         Member findMember = em.find(Member.class, member.getId());// id로 조회해서 꺼내겠다
-//        Team findTime = findMember.getTeam();//바로 꺼낼 수 있음.(연관관계 매핑때문에)
-//        System.out.println("findTeam = "+findTime.getName()); // findTeam = TeamA
+        Team findTime = findMember.getTeam();//바로 꺼낼 수 있음.(연관관계 매핑때문에)
+        System.out.println("findTeam = "+findTime.getName()); // findTeam = TeamA
 
         //양방향 연관관계(멤버에서 팀으로, 팀에서 멤버로 왔다갔다)
         List<Member> members = findMember.getTeam().getMembers();
@@ -44,6 +54,11 @@ public class JpaMain {
         for (Member m:members){
             System.out.println("m= "+m.getUsername());
         }
+
+        //영속성 수정
+     /*   Member updateM = em.find(Member.class, 2L);
+        updateM.setUsername("고현정");
+*/
         tx.commit();
     }catch (Exception e){
         //문제가 생기면 롤백
