b7bdcc78ad8319dc89857283455eac4b140035ec
KOHYEONJEONG <080821@naver.com>
2024-07-31 21:15:33 +0900

    JPA - 단방향 연관관계(Member4,Team4)

---
+++ src/main/java/chat4/Team4.java
@@ -0,0 +1,35 @@
+package chat4;
+
+import chat2.Member;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team4 {
+    @Id
+    @GeneratedValue
+    @Column(name="TEAM_ID")
+    private Long id;
+    private String name;
+
+    public Team4() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
